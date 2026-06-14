be285170e8eeed6b649b5f3ad48dd04c8bbf02a9
TheTuan14 <thetuan1407@gmail.com>
2026-06-14 17:51:02 +0700

    Add NguoiDungModule to AppModule imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TopicsModule } from './topics/topics.module';
 import { DatabaseModule } from './database/database.module';
+import { NguoiDungModule } from './nguoi-dung/nguoi-dung.module';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [NguoiDungModule],
 })
 export class AppModule {}
