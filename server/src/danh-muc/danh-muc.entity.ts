e8a1f7532b0cab66a12ef1434093a4d593e018a4
DoTrongThang17 <thangdo2006td@gmail.com>
2026-06-14 17:30:30 +0700

    Create danh-muc.entity.ts

---
+++ server/src/danh-muc/danh-muc.entity.ts
@@ -0,0 +1,62 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { NguoiDung } from '../nguoi-dung/nguoi-dung.entity';
+import { GiaoDich } from '../giao-dich/giao-dich.entity';
+import { NganSach } from '../ngan-sach/ngan-sach.entity';
+
+export enum LoaiDanhMuc {
+  THU = 'thu',
+  CHI = 'chi',
+}
+
+@Entity('DanhMuc')
+export class DanhMuc {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'nguoi_dung_id', nullable: true })
+  nguoiDungId: number | null;
+
+  @Column({ name: 'ten_danh_muc', length: 100 })
+  tenDanhMuc: string;
+
+  @Column({ type: 'text', nullable: true })
+  moTa: string;
+
+  @Column({ type: 'enum', enum: LoaiDanhMuc, default: LoaiDanhMuc.CHI })
+  loai: LoaiDanhMuc;
+
+  @Column({ name: 'mau_sac', length: 10, nullable: true })
+  mauSac: string;
+
+  @Column({ name: 'bieu_tuong', length: 50, nullable: true })
+  bieuTuong: string;
+
+  @CreateDateColumn({ name: 'ngay_tao' })
+  ngayTao: Date;
+
+  @UpdateDateColumn({ name: 'ngay_cap_nhat' })
+  ngayCapNhat: Date;
+
+  // Relations
+  @ManyToOne(() => NguoiDung, (nd) => nd.danhMucs, {
+    nullable: true,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'nguoi_dung_id' })
+  nguoiDung: NguoiDung;
+
+  @OneToMany(() => GiaoDich, (gd) => gd.danhMuc)
+  giaoDichs: GiaoDich[];
+
+  @OneToMany(() => NganSach, (ns) => ns.danhMuc)
+  nganSachs: NganSach[];
+}
